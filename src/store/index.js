37f2c681d8ee6f1545cfe6ab09bbd3a2374b232d
772282161 <772282161@qq.com>
2021-03-08 23:17:36 +0800

    redux thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
-import { createStore } from 'redux'; //引入createStore方法
+import { createStore ,applyMiddleware , compose} from 'redux'; //引入createStore方法
 import reducer from './reducer';//把reducer 引入
-const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); //创建数据存储仓库
+import thunk from 'redux-thunk'
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__?
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}):compose
+
+const enhancer = composeEnhancer(applyMiddleware(thunk))
+
+const store = createStore(reducer,enhancer ); //创建数据存储仓库
 
 
 
